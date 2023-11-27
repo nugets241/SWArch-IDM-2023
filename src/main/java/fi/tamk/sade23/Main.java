01109f14fdd61a1090c4a66d10893153770cd207
sobrepenaej@yahoo.com <sobrepenaej@yahoo.com>
2023-11-27 18:08:54 +0200

    Implemented Design 3.0: Created separate Contacts Service with simplified attributes

--- src/main/java/fi/tamk/sade23/Main.java
+++ src/main/java/fi/tamk/sade23/Main.java
@@ -1,24 +1,19 @@
 package fi.tamk.sade23;
 
 import fi.tamk.sade23.idm.*;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        Staff staff = new Staff("Bob", "Marley", "Staff", "Reggae University", "2023-12-31", "bob.marley@example.com", "Professor");
-        staff.setStrategy(new StaffAttributeStrategy());
-        staff.applyStrategy();
-        System.out.println("Staff: " + staff.getFirstName() + " " + staff.getLastName() + ", " + staff.getStaffSpecificAttribute());
+        ContactsService contactsService = new ContactsService();
 
-        Student student = new Student("Alice", "Cooper", "Student", "Rock University", "2023-06-30", "alice.cooper@example.com", "Undergraduate");
-        student.setStrategy(new StudentAttributeStrategy());
-        student.applyStrategy();
-        System.out.println("Student: " + student.getFirstName() + " " + student.getLastName() + ", " + student.getStudentSpecificAttribute());
+        Contact bob = contactsService.createContact("Bob", "Marley", "bob.marley@example.com", "+123456789");
+        System.out.println("Contact: " + bob.getFirstName() + " " + bob.getLastName() + ", " + bob.getEmail());
 
-        Visitor visitor = new Visitor("Jimmy", "Page", "Visitor", "Blues College", "2023-03-31", "jimmy.page@example.com", "Guest Lecturer");
-        visitor.setStrategy(new VisitorAttributeStrategy());
-        visitor.applyStrategy();
-        System.out.println("Visitor: " + visitor.getFirstName() + " " + visitor.getLastName() + ", " + visitor.getVisitorSpecificAttribute());
+        Contact alice = contactsService.createContact("Alice", "Cooper", "alice.cooper@example.com", "+987654321");
+        System.out.println("Contact: " + alice.getFirstName() + " " + alice.getLastName() + ", " + alice.getEmail());
+
+        Contact jimmy = contactsService.createContact("Jimmy", "Page", "jimmy.page@example.com", "+123987456");
+        System.out.println("Contact: " + jimmy.getFirstName() + " " + jimmy.getLastName() + ", " + jimmy.getEmail());
     }
 }
---
+++ src/main/java/fi/tamk/sade23/idm/ContactsService.java
@@ -0,0 +1,25 @@
+package fi.tamk.sade23.idm;
+
+import java.util.HashMap;
+
+public class ContactsService {
+    private HashMap<Integer, Contact> contacts = new HashMap<>();
+    static private Integer idCounter = 1;
+
+    public Contact createContact(String firstName, String lastName, String email, String phoneNumber) {
+        Contact contact = new Contact(firstName, lastName, email, phoneNumber);
+        contacts.put(idCounter, contact);
+
+        idCounter++;
+        return contact;
+    }
+
+    public Contact getContact(Integer id) {
+        return contacts.get(id);
+    }
+
+    public void modifyContact(Integer id, String firstName, String lastName, String email, String phoneNumber) {
+        Contact contact = new Contact(firstName, lastName, email, phoneNumber);
+        contacts.put(id, contact);
+    }
+}
